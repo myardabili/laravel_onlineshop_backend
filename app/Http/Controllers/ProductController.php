9979d6df037962baf955bcaaaba3bbe8cac17343
myusufardabili <muhammadyusufnew16@gmail.com>
2024-02-22 10:32:06 +0700

    first commit, user, category, product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = \App\Models\Product::paginate(5);
+        return view('pages.product.index', compact('products'));
+    }
+
+    public function create(){
+        $categories = \App\Models\Category::all();
+        return view('pages.product.create', compact('categories'));
+    }
+
+    public function store(Request $request){
+        // $data = $request->all();
+        // $data['image'] = $request->file('image')->store('assets/product', 'public');
+        // \App\Models\Product::create($data);
+        $filename = time() . '.' . $request->image->extension();
+        $request->image->storeAs('public/products', $filename);
+        // $data = $request->all();
+
+        $product = new \App\Models\Product;
+        $product->name = $request->name;
+        $product->price = (int) $request->price;
+        $product->stock = (int) $request->stock;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+        $product->image = $filename;
+        $product->save();
+
+        return redirect()->route('product.index');
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(9)->create();
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin Yusuf',
+            'email' => 'yusuf@fic12.com',
+            'password' => Hash::make('12345678'),
+            'phone' => '088226684082',
+            'roles' => 'ADMIN',
+        ]);
+    }
+}
